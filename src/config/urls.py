d6c44be8fa1f30dc8a436fc32c4fb34828996977
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-06-06 19:35:21 -0300

    feat: adiciona base.html, dashboard e custom.css com Bootstrap

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,14 @@
 """Root URL configuration."""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
+    path('academico/', include('academico.urls', namespace='academico')),
+    path('pessoas/', include('pessoas.urls', namespace='pessoas')),
+    path('infraestrutura/', include('infraestrutura.urls', namespace='infraestrutura')),
+    path('alocacao/', include('alocacao.urls', namespace='alocacao')),
+    path('relatorios/', include('relatorios.urls', namespace='relatorios')),
 ]
