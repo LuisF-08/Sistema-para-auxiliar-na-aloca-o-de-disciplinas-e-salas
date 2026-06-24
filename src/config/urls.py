47c311739e28d8c00f5c1fa822d53ae5dec00530
Mateus Coelho <cm.mateuscoelho@gmail.com>
2026-06-24 01:31:49 -0300

    Revert "feat(relatorios): implementa módulo de dashboard e relatórios"
    This reverts commit f96f30276c682474744f6fa92906cd6e31141783.

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pessoas/', include('pessoas.urls')),
-    path('academico/', include('academico.urls')),
-    path('relatorios/', include('relatorios.urls')),
+    path('pessoas/', include('pessoas.urls')),     # Conecta as rotas de professores
+    path('academico/', include('academico.urls')), # Conecta as rotas de turmas
 ]
