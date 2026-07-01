29b2736f2a320a44ea6e40f48ee0e5a8f5e0151d
kauanmirante <kauanmirante@gmail.com>
2026-06-30 23:23:49 -0300

    Update views.py

--- src/alocacao/views.py
+++ src/alocacao/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from academico.models import PeriodoLetivo
+from alocacao.models import Alocacao
 
 def alocacao_list(request):
-    return render(request, 'alocacao/alocacao_list.html', {})
+    periodos = PeriodoLetivo.objects.filter(ativo=True).order_by('-ano', '-semestre')
+    periodo_selecionado_id = request.GET.get('periodo')
+    
+    if periodo_selecionado_id:
+        periodo_atual = get_object_or_404(PeriodoLetivo, pk=periodo_selecionado_id)
+    else:
+        periodo_atual = periodos.first()
+
+    alocacoes = Alocacao.objects.filter(
+        periodo_letivo=periodo_atual
+    ).select_related(
+        'professor', 
+        'disciplina', 
+        'turma', 
+        'sala', 
+        'horario'
+    )
+
+    context = {
+        'periodos': periodos,
+        'periodo_atual': periodo_atual,
+        'alocacoes': alocacoes,
+    }
+    
+    return render(request, 'alocacao/alocacao_list.html', context)
