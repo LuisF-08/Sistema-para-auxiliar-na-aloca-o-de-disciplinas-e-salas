3ac760404edb2181e119599069f1daea0343dcbd
Dann2405 <danielsoares0558@gmail.com>
2026-07-07 20:50:08 -0300

    fix: corrige NameError e reverse sem namespace nas views de pessoas e academico

--- src/academico/views.py
+++ src/academico/views.py
@@ -12,17 +12,17 @@ class TurmaCreateView(CreateView):
     model = Turma
     fields = '__all__'
     template_name = 'academico/turma_form.html'
-    success_url = reverse_lazy('turma_create')
+    success_url = reverse_lazy('academico:turma_create')
 
 class TurmaUpdateView(UpdateView):
     model = Turma
     fields = '__all__'
     context_object_name = 'turma'
     template_name = 'academico/turma_form.html'
-    success_url = reverse_lazy('turma_create')
+    success_url = reverse_lazy('academico:turma_create')
 
 class TurmaDeleteView(DeleteView):
     model = Turma
     context_object_name = 'turma'
     template_name = 'academico/turma_confirm_delete.html'
-    success_url = reverse_lazy('turma_create')
+    success_url = reverse_lazy('academico:turma_create')
