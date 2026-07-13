2439cf6978f89498ef4bf4ab6b517951195c2096
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-07-13 17:11:15 -0300

    feat(infraestrutura): Finalização e teste da implementacao do CRUD completo de Salas via modal

--- src/infraestrutura/urls.py
+++ src/infraestrutura/urls.py
@@ -5,4 +5,7 @@ app_name = 'infraestrutura'
 
 urlpatterns = [
     path('salas/', views.sala_list, name='sala_list'),
+    path('salas/novo/', views.sala_create, name='sala_create'),
+    path('salas/<int:pk>/editar/', views.sala_update, name='sala_update'),
+    path('salas/<int:pk>/excluir/', views.sala_delete, name='sala_delete')
 ]
