aa98a577a9e904e45e137298d59b0f38378c614d
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-07-13 16:47:08 -0300

    feat(pessoas): Finalização e teste da implementacao do CRUD completo de Turma e Professor via modal

--- src/pessoas/urls.py
+++ src/pessoas/urls.py
@@ -5,5 +5,12 @@ app_name = 'pessoas'
 
 urlpatterns = [
     path('turmas/', views.turma_list, name='turma_list'),
+    path('turmas/nova/', views.turma_create, name='turma_create'),
+    path('turmas/<int:pk>/editar/', views.turma_update, name='turma_update'),
+    path('turmas/<int:pk>/excluir/', views.turma_delete, name='turma_delete'),
+
     path('professores/', views.professor_list, name='professor_list'),
+    path('professores/nova/', views.professor_create, name='professor_create'),
+    path('professores/<int:pk>/editar/', views.professor_update, name='professor_update'),
+    path('professores/<int:pk>/excluir/', views.professor_delete, name='professor_delete'),
 ]
--- src/pessoas/views.py
+++ src/pessoas/views.py
@@ -1,11 +1,11 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DeleteView
-from .models import Professor
-from django.shortcuts import redirect, render
+from django.db import IntegrityError
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib import messages
 from .models import Turma
+from .models import Professor
 from academico.models import Curso, PeriodoLetivo
     
-def turma_list(request):
+def turma_create(request):
 
     if request.method == 'POST':
         nome = request.POST.get('nome')
@@ -55,31 +55,103 @@ def turma_list(request):
         'periodos': periodos
     })
 
+def turma_list(request):
+    turmas = Turma.objects.all()
+    cursos = Curso.objects.all()
+    periodos = PeriodoLetivo.objects.all()
+    
+    return render(request, 'pessoas/turma_list.html', {
+        'turmas': turmas,
+        'cursos': cursos,
+        'periodos': periodos
+    })
 
 def turma_update(request, pk):
-    turma = Turma.objects.get(pk=pk)
-    if request.method == 'POST':
+   turma = get_object_or_404(Turma, pk=pk)
+
+   if request.method == 'POST':
         turma.nome = request.POST.get('nome')
         curso_id = request.POST.get('curso')
-        periodo_letivo = request.POST.get('periodo_letivo')
-        turma.numero_alunos = request.POST.get('numero_alunos')
+        periodo_id = request.POST.get('periodo_letivo')
+        numero_alunos = request.POST.get('numero_alunos')
         turma.turno = request.POST.get('turno')
 
-        curso_instancia = Curso.objects.get(id=curso_id) if curso_id else None
-        periodo_instancia = None
+        turma.curso = get_object_or_404(Curso, pk=curso_id) if curso_id else None
+        turma.periodo_letivo = get_object_or_404(PeriodoLetivo, pk=periodo_id) if periodo_id else None
+        turma.numero_alunos = int(numero_alunos) if numero_alunos else 0
 
-        if periodo_letivo:
-            if str(periodo_letivo).isdigit():
-                periodo_instancia = PeriodoLetivo.objects.filter(id=periodo_letivo).first()
-            else:
-                apenas_numeros = ''.join(c for c in str(periodo_letivo) if c.isdigit())
-                if apenas_numeros:
-                    semestre_num = int(apenas_numeros)
-                    periodo_instancia = PeriodoLetivo.objects.filter(semestre=semestre_num).first()
-
-        turma.curso = curso_instancia
-        turma.periodo_letivo = periodo_instancia
         turma.save()
+        messages.success(request, 'Turma atualizada com sucesso!')
+        return redirect('pessoas:turma_list')  
+
+   return redirect('pessoas:turma_list')
+        
+def turma_delete(request, pk):
+    turma = get_object_or_404(Turma, pk=pk)
+    if request.method == 'POST':
+        turma.delete()
+        messages.success(request, 'Turma excluída com sucesso!')
+    return redirect('pessoas:turma_list')  
 
 def professor_list(request):
-    return render(request, 'pessoas/professor_list.html', {})
+    professores = Professor.objects.all()
+    return render(request, 'pessoas/professor_list.html', {'professores': professores})
+
+def professor_create(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        especialidade = request.POST.get('especialidade')
+        carga_horaria_maxima  = request.POST.get('cargaHorariaMaxima')
+        ativo = request.POST.get('ativo') == 'on'
+        try:
+            Professor.objects.create(
+                nome=nome,
+                email=email,
+                especialidade=especialidade,
+                carga_horaria_maxima=int(carga_horaria_maxima) if carga_horaria_maxima else None,
+                ativo=ativo
+            )
+            messages.success(request, 'Professor cadastrado com sucesso!')
+        except IntegrityError:
+            messages.error(request, f'Já existe um professor cadastrado com o e-mail "{email}".')
+
+        
+        return redirect('/pessoas/professores/')
+
+    professores = Professor.objects.all()
+    
+    return render(request, 'pessoas/professor_list.html', {
+        'professores': professores
+    })
+
+
+def professor_update(request, pk):
+    professor = get_object_or_404(Professor, pk=pk)
+
+    if request.method == 'POST':
+        professor.nome = request.POST.get('nome')
+        professor.email = request.POST.get('email')
+        professor.especialidade = request.POST.get('especialidade')
+        carga_horaria_maxima = request.POST.get('cargaHorariaMaxima')
+        professor.carga_horaria_maxima = int(carga_horaria_maxima) if carga_horaria_maxima else 10
+        professor.ativo = request.POST.get('ativo') == 'on'
+
+
+        try:
+            professor.save()
+            messages.success(request, 'Professor atualizado com sucesso!')
+        except IntegrityError:
+            messages.error(request, f'Já existe um professor cadastrado com o e-mail "{professor.email}".')
+            
+        return redirect('pessoas:professor_list')
+    
+    return render(request, 'pessoas/professor_form.html', {'professor': professor})
+
+
+def professor_delete(request, pk):
+    professor = get_object_or_404(Professor, pk=pk)
+    if request.method == 'POST':
+        professor.delete()
+        messages.success(request, 'Professor excluído com sucesso!')
+    return redirect('pessoas:professor_list')
